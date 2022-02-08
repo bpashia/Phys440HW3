8bfb625a1045ab8c38f9babef4677374933c78ff
bpashia <bbpashia@gmail.com>
2022-02-08 16:16:00 -0600

    Initial Commit

---
+++ Shared/Hw3BrocPashiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hw3BrocPashiaApp.swift
+//  Shared
+//
+//  Created by Broc Pashia on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Hw3BrocPashiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
